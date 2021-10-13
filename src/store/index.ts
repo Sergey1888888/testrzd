a6e8841df483796c1e51d947b4897c8dd0bca048
sergey1888888 <sergey1888888@gmail.com>
2021-10-13 04:26:51 +0300

    redux-devtools deleted

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,5 @@
-import {applyMiddleware, compose, createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {rootReducer} from "./reducers";
 import thunk from "redux-thunk";
 
-// @ts-ignore
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const enhancer = composeEnhancers(
-    applyMiddleware(thunk),
-    // other store enhancers if any
-);
-
-// @ts-ignore
-export const store = createStore(rootReducer, enhancer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
